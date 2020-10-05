42dc730e562605720f483de26c204f6e82469128
Tony <tr33947@gmail.com>
2020-10-05 09:42:05 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var akanMaleName= ["Kwasi!", "Kwadwo!", "Kwabena!", "Kwaku!", "Yaw!", "Kofi!", "Kwame!"]
-var akanFemaleName= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var akanMaleName= ["Kwasi!", "Kwadwo!", "Kwabena!", "Kwaku!", "Yaw!", "Kofi!", "Kwame!"];
+var akanFemaleName= ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 function checkDayOfMonth(){
   var century = parseInt(document.getElementById("century").value);
